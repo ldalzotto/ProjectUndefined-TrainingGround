6533ce01e952c73f167b805a7d9557848aa35040
LDZ <loic.dalzotto@hotmail.fr>
2019-02-08 10:28:41 +0100

    REFACTORING - Preparing the implementation of RTP action wheel

---
+++ Assets/~RTPuzzleGame/PlayerAction/ActionExecution/Scripts/Actions/LaunchProjectileRTPAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LaunchProjectileRTPAction : RTPPlayerAction
+{
+    public override bool FinishedCondition()
+    {
+        return true;
+    }
+
+    public override void FirstExecution()
+    {
+        Debug.Log("Dummy LaunchProjectileAction start.");
+    }
+
+    public override void Tick(float d)
+    {
+        Debug.Log("Dummy LaunchProjectileAction tick.");
+    }
+}
