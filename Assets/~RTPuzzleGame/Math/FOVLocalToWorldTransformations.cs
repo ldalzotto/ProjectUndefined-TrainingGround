528952d909be3ee3224a7c1e1210d5a2214b6431
LDZ <loic.dalzotto@hotmail.fr>
2019-02-26 14:34:15 +0100

    FIX - FOV local to world taking into account the world axis

--- Assets/~RTPuzzleGame/Math/FOVLocalToWorldTransformations.cs
+++ Assets/~RTPuzzleGame/Math/FOVLocalToWorldTransformations.cs
@@ -6,11 +6,24 @@ namespace RTPuzzle
     public class FOVLocalToWorldTransformations
     {
 
-
-        public static float AngleFromDirectionInFOVWorld(Vector3 localDirection, NavMeshAgent agent)
+        /// <summary>
+        /// <para>
+        /// The FOV space is world space vector <see cref="Vector3.forward"/>.
+        /// </para>
+        /// <para>
+        /// The <paramref name="localEscapeDirection"/> is projected to FOV space.
+        /// Then, the angle between <see cref="Vector3.forward"/> and the projected escape direction is computed.
+        /// </para>
+        /// 
+        /// </summary>
+        /// <param name="localEscapeDirection"></param>
+        /// <param name="agent"></param>
+        /// <returns></returns>
+        public static float AngleFromDirectionInFOVSpace(Vector3 localEscapeDirection, NavMeshAgent agent)
         {
-            var localDirectionProjected = Vector3.ProjectOnPlane(localDirection, agent.transform.up);
-            return Vector3.SignedAngle(localDirectionProjected, Vector3.forward, agent.transform.up);
+            //TODO -> is reacting correctly with slopes ?
+            var localDirectionProjected = Vector3.ProjectOnPlane(localEscapeDirection, Vector3.up);// agent.transform.up);
+            return Vector3.SignedAngle(localDirectionProjected, Vector3.forward, Vector3.up);// agent.transform.up);
         }
 
     }
