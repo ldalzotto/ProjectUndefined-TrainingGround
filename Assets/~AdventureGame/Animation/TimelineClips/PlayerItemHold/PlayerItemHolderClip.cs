06cfd3f4c2ab789e5b565960b6b1a158fd635369
LDZ <loic.dalzotto@hotmail.fr>
2019-04-18 09:30:10 +0200

    Preparing update to 2019.1

--- Assets/~AdventureGame/Animation/TimelineClips/PlayerItemHold/PlayerItemHolderClip.cs
+++ Assets/~AdventureGame/Animation/TimelineClips/PlayerItemHold/PlayerItemHolderClip.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.Playables;
-using UnityEngine.Timeline;
 
 namespace AdventureGame
 {
-    public class PlayerItemHolderClip : PlayableAsset, ITimelineClipAsset
+    public class PlayerItemHolderClip : PlayableAsset
     {
 
         public PlayerItemHolderBehavior PlayerItemHolderBehavior = new PlayerItemHolderBehavior();
-
-        public ClipCaps clipCaps => ClipCaps.None;
 
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
