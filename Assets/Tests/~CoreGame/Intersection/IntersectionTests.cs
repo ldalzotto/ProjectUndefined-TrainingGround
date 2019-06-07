42f822936ebdd8d3f27ba44f7602dcbce9a6b300
LDZ <loic.dalzotto@hotmail.fr>
2019-06-07 09:13:52 +0200

    FEATURE - Totally accurate projectile collision detection

---
+++ Assets/Tests/~CoreGame/Intersection/IntersectionTests.cs
@@ -0,0 +1,76 @@
+﻿using CoreGame;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class IntersectionTests : MonoBehaviour
+    {
+
+        private BoxCollider CreateBoxCollider()
+        {
+            var go = new GameObject();
+            go.transform.position = Vector3.zero;
+            go.transform.localScale = Vector3.one;
+            go.transform.rotation = Quaternion.identity;
+            var box= go.AddComponent<BoxCollider>();
+            box.transform.position = Vector3.zero;
+            box.transform.localScale = Vector3.one;
+            box.transform.rotation = Quaternion.identity;
+            return box;
+        }
+
+        private void InitBox(ref BoxCollider box, Vector3 size, Quaternion quaternion)
+        {
+            box.size = size;
+            box.transform.rotation = quaternion;
+        }
+
+        [Test]
+        public void NonRotateCube()
+        {
+            var sphereWorldPosition = Vector3.zero;
+            var sphereRadius = 0.5f;
+
+            BoxCollider box = this.CreateBoxCollider();
+            this.InitBox(ref box, Vector3.one, Quaternion.identity);
+            
+            this.AssertBox(Vector3.forward, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.back, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.up, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.down, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.left, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.right, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+        }
+
+        [Test]
+        public void WithRotateCube()
+        {
+            var sphereWorldPosition = Vector3.zero;
+            var sphereRadius = 0.5f;
+
+            BoxCollider box = this.CreateBoxCollider();
+            this.InitBox(ref box, Vector3.one, Quaternion.Euler(90f,90f,90f));
+            
+            this.AssertBox(Vector3.forward, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.back, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.up, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.down, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.left, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+            this.AssertBox(Vector3.right, 0.1f, ref box, sphereWorldPosition, sphereRadius);
+        }
+
+        private void AssertBox(Vector3 moveDirection, float directionDelta, ref BoxCollider box, Vector3 sphereWorldPosition, float sphereRadius) {
+
+            box.transform.position = moveDirection;
+            Assert.IsTrue(Intersection.BoxIntersectsSphere(box, sphereWorldPosition, sphereRadius));
+
+            box.transform.position = moveDirection + (moveDirection * (-1 * directionDelta));
+            Assert.IsTrue(Intersection.BoxIntersectsSphere(box, sphereWorldPosition, sphereRadius));
+
+            box.transform.position = moveDirection + (moveDirection * directionDelta);
+            Assert.IsFalse(Intersection.BoxIntersectsSphere(box, sphereWorldPosition, sphereRadius));
+        }
+    }
+
+}
--- Assets/~CoreGame/Math/Intersection.cs
+++ Assets/~CoreGame/Math/Intersection.cs
@@ -5,52 +5,76 @@ namespace CoreGame
     public class Intersection
     {
 
-        public static bool BoxIntersectsSphere(BoxCollider boxCollider, Vector3 S, float R)
+        public static bool BoxIntersectsSphere(BoxCollider boxCollider, Vector3 SphereWorldPosition, float SphereRadius)
         {
             var boxPosition = boxCollider.transform.position + boxCollider.center;
 
             Vector3 diagDirection = new Vector3(boxCollider.size.x, boxCollider.size.y, boxCollider.size.z);
             diagDirection.Scale(boxCollider.transform.lossyScale);
 
-            var C1 = (boxPosition - boxCollider.transform.rotation * (diagDirection / 2f));
-            var C2 = (boxPosition + boxCollider.transform.rotation * (diagDirection / 2f));
+            Vector3 C1 = (boxPosition - boxCollider.transform.rotation * (diagDirection / 2f));
+            Vector3 C2 = (boxPosition + boxCollider.transform.rotation * (diagDirection / 2f));
 
             diagDirection = new Vector3(-boxCollider.size.x, -boxCollider.size.y, boxCollider.size.z);
             diagDirection.Scale(boxCollider.transform.lossyScale);
 
-            var C3 = (boxPosition - boxCollider.transform.rotation * (diagDirection / 2f));
-            var C4 = (boxPosition + boxCollider.transform.rotation * (diagDirection / 2f));
+            Vector3 C3 = (boxPosition - boxCollider.transform.rotation * (diagDirection / 2f));
+            Vector3 C4 = (boxPosition + boxCollider.transform.rotation * (diagDirection / 2f));
 
             diagDirection = new Vector3(-boxCollider.size.x, boxCollider.size.y, boxCollider.size.z);
             diagDirection.Scale(boxCollider.transform.lossyScale);
 
-            var C5 = (boxPosition - boxCollider.transform.rotation * (diagDirection / 2f));
-            var C6 = (boxPosition + boxCollider.transform.rotation * (diagDirection / 2f));
+            Vector3 C5 = (boxPosition - boxCollider.transform.rotation * (diagDirection / 2f));
+            Vector3 C6 = (boxPosition + boxCollider.transform.rotation * (diagDirection / 2f));
 
             diagDirection = new Vector3(boxCollider.size.x, -boxCollider.size.y, boxCollider.size.z);
             diagDirection.Scale(boxCollider.transform.lossyScale);
 
-            var C7 = (boxPosition - boxCollider.transform.rotation * (diagDirection / 2f));
-            var C8 = (boxPosition + boxCollider.transform.rotation * (diagDirection / 2f));
+            Vector3 C7 = (boxPosition - boxCollider.transform.rotation * (diagDirection / 2f));
+            Vector3 C8 = (boxPosition + boxCollider.transform.rotation * (diagDirection / 2f));
 
-            float dist_squared = R * R;
-            if (DiagonalCheck(S, C1, C2, R * R) > 0) { return true; }
-            else if (DiagonalCheck(S, C3, C4, R * R) > 0) { return true; }
-            else if (DiagonalCheck(S, C5, C6, R * R) > 0) { return true; }
-            else if (DiagonalCheck(S, C7, C8, R * R) > 0) { return true; }
+            float dist_squared = SphereRadius * SphereRadius;
+            if (DiagonalCheck(SphereWorldPosition, C1, C2, SphereRadius)) { return true; }
+            else if (DiagonalCheck(SphereWorldPosition, C3, C4, SphereRadius)) { return true; }
+            else if (DiagonalCheck(SphereWorldPosition, C5, C6, SphereRadius)) { return true; }
+            else if (DiagonalCheck(SphereWorldPosition, C7, C8, SphereRadius)) { return true; }
             return false;
         }
 
-        private static float DiagonalCheck(Vector3 S, Vector3 point1, Vector3 point2, float dist_squared)
+        private static bool DiagonalCheck(Vector3 S, Vector3 point1, Vector3 point2, float radius)
         {
-            if (S.x < point1.x) dist_squared -= Mathf.Pow(S.x - point1.x, 2);
-            else if (S.x > point2.x) dist_squared -= Mathf.Pow(S.x - point2.x, 2);
-            if (S.y < point1.y) dist_squared -= Mathf.Pow(S.y - point1.y, 2);
-            else if (S.y > point2.y) dist_squared -= Mathf.Pow(S.y - point2.y, 2);
-            if (S.z < point1.z) dist_squared -= Mathf.Pow(S.z - point1.z, 2);
-            else if (S.z > point2.z) dist_squared -= Mathf.Pow(S.z - point2.z, 2);
-            return dist_squared;
+
+            //X projection
+            bool xProjectionInside = false;
+            float minX = (float)(System.Math.Round((double)Mathf.Min(point1.x, point2.x), 3));
+            float maxX = (float)(System.Math.Round((double)Mathf.Max(point1.x, point2.x), 3));
+
+            if (minX >= S.x - radius && minX <= S.x + radius) { xProjectionInside = true; }
+            if (maxX >= S.x - radius && maxX <= S.x + radius) { xProjectionInside = true; }
+            if (minX <= S.x - radius && maxX >= S.x + radius) { xProjectionInside = true; }
+
+            //Y projection
+            bool yProjectionInside = false;
+            float minY = (float)(System.Math.Round((double)Mathf.Min(point1.y, point2.y), 3));
+            float maxY = (float)(System.Math.Round((double)Mathf.Max(point1.y, point2.y), 3));
+
+            if (minY >= S.y - radius && minY <= S.y + radius) { yProjectionInside = true; }
+            if (maxY >= S.y - radius && maxY <= S.y + radius) { yProjectionInside = true; }
+            if (minY <= S.y - radius && maxY >= S.y + radius) { yProjectionInside = true; }
+
+            //Z projection
+            bool zProjectionInside = false;
+            float minZ = (float)(System.Math.Round((double)Mathf.Min(point1.z, point2.z), 3));
+            float maxZ = (float)(System.Math.Round((double)Mathf.Max(point1.z, point2.z), 3));
+
+            if (minZ >= S.z - radius && minZ <= S.z + radius) { zProjectionInside = true; }
+            if (maxZ >= S.z - radius && maxZ <= S.z + radius) { zProjectionInside = true; }
+            if (minZ <= S.z - radius && maxZ >= S.z + radius) { zProjectionInside = true; }
+
+            return (xProjectionInside && yProjectionInside && zProjectionInside);
         }
+
+
     }
 
 }
