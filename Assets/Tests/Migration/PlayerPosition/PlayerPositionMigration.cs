65b5e70f86320ff3ea73eafab43557039e3f9c92
LDZ <loic.dalzotto@hotmail.fr>
2019-07-01 14:16:11 +0200

    MIGRATION - Copy and pasting player transform from test scenes

---
+++ Assets/Tests/Migration/PlayerPosition/PlayerPositionMigration.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPositionMigration : EditorWindow
+{
+    [MenuItem("Migration/PlayerPositionMigration")]
+    static void Init()
+    {
+        PlayerPositionMigration window = (PlayerPositionMigration)EditorWindow.GetWindow(typeof(PlayerPositionMigration));
+        window.Show();
+    }
+
+    private Dictionary<string, TransformBinarry> playerPositions;
+
+    private void OnEnable()
+    {
+        this.playerPositions = new Dictionary<string, TransformBinarry>();
+    }
+
+    private const string positionFilePath = "C:\\Users\\Loic\\Documents\\projects\\SoulsLike\\No Entry_0.0.71_Reset\\Assets\\Tests\\Migration\\PlayerPosition\\SavedPosition.txt";
+
+    private void OnGUI()
+    {
+        EditorGUI.BeginDisabledGroup(true);
+        if (GUILayout.Button("FIND POSITIONS"))
+        {
+            this.playerPositions.Clear();
+            for (var i = 9; i < 14; i++)
+            {
+                var scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+                Debug.Log(scenePath);
+                EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+                var playerObject = GameObject.FindObjectOfType<RTPuzzle.PlayerManager>();
+                if (playerObject != null)
+                {
+                    this.playerPositions.Add(scenePath, new TransformBinarry(playerObject.transform));
+                }
+            }
+        }
+        if (GUILayout.Button("SAVE POSITIONS"))
+        {
+            List<PlayerPositionBinarry> positionsToSave = new List<PlayerPositionBinarry>();
+            Debug.Log(this.playerPositions.Count);
+            foreach (var playerPosition in this.playerPositions)
+            {
+                positionsToSave.Add(new PlayerPositionBinarry(playerPosition.Key, playerPosition.Value));
+            }
+
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (FileStream fileStream = File.Open(positionFilePath, FileMode.OpenOrCreate))
+            {
+                binaryFormatter.Serialize(fileStream, positionsToSave);
+            }
+        }
+
+
+        EditorGUI.EndDisabledGroup();
+
+        EditorGUILayout.Separator();
+        EditorGUILayout.LabelField("UPDATE PROJECT...");
+        EditorGUILayout.Separator();
+
+        if (GUILayout.Button("MIGRATE SCENES"))
+        {
+            List<PlayerPositionBinarry> positionsToSave = null;
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (FileStream fileStream = File.Open(positionFilePath, FileMode.Open))
+            {
+                positionsToSave = (List<PlayerPositionBinarry>)binaryFormatter.Deserialize(fileStream);
+            }
+
+            if (positionsToSave != null)
+            {
+                for (var i = 9; i < 14; i++)
+                {
+                    var scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+                    EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+                    var playerObject = GameObject.FindObjectOfType<RTPuzzle.PlayerManager>();
+                    if (playerObject != null)
+                    {
+                        foreach (var positionToSave in positionsToSave)
+                        {
+                            if (positionToSave.ScenePath == scenePath)
+                            {
+                                var formattedTransform = positionToSave.Transform.Format();
+                                playerObject.transform.position = formattedTransform.position;
+                                playerObject.transform.rotation = formattedTransform.rotation;
+                                playerObject.transform.localScale = formattedTransform.localScale;
+
+                                EditorUtility.SetDirty(playerObject);
+                                EditorSceneManager.SaveOpenScenes();
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        if (GUILayout.Button("DELETE SAVED DATA"))
+        {
+            File.Delete(positionFilePath);
+        }
+    }
+
+    [System.Serializable]
+    public struct PlayerPositionBinarry
+    {
+        [SerializeField]
+        public string ScenePath;
+
+        [SerializeField]
+        public TransformBinarry Transform;
+
+        public PlayerPositionBinarry(string scenePath, TransformBinarry transform)
+        {
+            ScenePath = scenePath;
+            Transform = transform;
+        }
+    }
+}
--- Assets/~CoreGame/Helpers/SerializationHelper.cs
+++ Assets/~CoreGame/Helpers/SerializationHelper.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 [System.Serializable]
 public struct Vector3Binarry
@@ -11,11 +10,16 @@ public struct Vector3Binarry
     [SerializeField]
     public float z;
 
-    public Vector3Binarry(Vector3 vector3) 
+    public Vector3Binarry(Vector3 vector3)
     {
         this.x = vector3.x;
         this.y = vector3.y;
         this.z = vector3.z;
+    }
+
+    public Vector3 ToVector3()
+    {
+        return new Vector3(x, y, z);
     }
 }
 
@@ -37,5 +41,52 @@ public struct QuaternionBinarry
         this.y = quaternion.y;
         this.z = quaternion.z;
         this.w = quaternion.w;
+    }
+
+    public Quaternion ToQuaternion()
+    {
+        return new Quaternion(x, y, z, w);
+    }
+}
+
+[System.Serializable]
+public struct TransformBinarry
+{
+    [SerializeField]
+    public Vector3Binarry position;
+    [SerializeField]
+    public QuaternionBinarry rotation;
+    [SerializeField]
+    public Vector3Binarry localScale;
+
+    public TransformBinarry(Transform transform)
+    {
+        this.position = new Vector3Binarry(transform.position);
+        this.rotation = new QuaternionBinarry(transform.rotation);
+        this.localScale = new Vector3Binarry(transform.localScale);
+    }
+
+    public TransformBinarryFormatted Format()
+    {
+        return new TransformBinarryFormatted(
+                this.position.ToVector3(),
+                this.rotation.ToQuaternion(),
+                this.localScale.ToVector3()
+            );
+    }
+
+}
+
+public struct TransformBinarryFormatted
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    public Vector3 localScale;
+
+    public TransformBinarryFormatted(Vector3 position, Quaternion rotation, Vector3 localScale)
+    {
+        this.position = position;
+        this.rotation = rotation;
+        this.localScale = localScale;
     }
 }
