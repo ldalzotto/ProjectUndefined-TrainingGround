6387f0b08c98905579470546d9d424c75d77e1c0
LDZ <loic.dalzotto@hotmail.fr>
2019-07-24 09:43:03 +0200

    FEATURE - Text id simplification

--- Assets/@GameConfigurationID/IDs/DiscussionNodeId.cs
+++ Assets/@GameConfigurationID/IDs/DiscussionNodeId.cs
@@ -8,42 +8,23 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GameConfigurationID {
-  
-  
-  [System.Serializable()]
-  public enum DiscussionNodeId {
-    
-    NONE = 0,
-    
-    BOUNCER_FORBIDDEN_INTRODUCTION = 1,
-    
-    BOUNCER_ASK_AGE = 2,
-    
-    BOUNCER_OK_INTRODUCTION = 3,
-    
-    PLAYER_AGE_CHOICE = 4,
-    
-    PLAYER_AGE_CHOICE_17 = 5,
-    
-    PLAYER_AGE_CHOICE_18 = 6,
-    
-    BOUNCER_GET_OUT = 7,
-    
-    SWER_RTP_1_DOOR = 9,
-    
-    _1_Town_GardenWatchman_Forbidden_NodeEditor_1 = 10,
-    
-    _1_Town_GardenWatchman_Forbidden_NodeEditor_2 = 11,
-    
-    _1_Town_GardenWatchman_Forbidden_NodeEditor_3 = 12,
-    
-    _1_Town_GardenWatchman_Forbidden_NodeEditor_4 = 13,
-    
-    _1_Town_GardenWatchman_Forbidden_NodeEditor_5 = 14,
-    
-    _1_Town_GardenWatchman_Forbidden_NodeEditor_6 = 15,
-    
-    _1_Town_GardenWatchman_Forbidden_NodeEditor_7 = 16,
-  }
+namespace GameConfigurationID
+{
+
+
+    [System.Serializable()]
+    public enum DiscussionNodeId
+    {
+
+        NONE = 0,
+        A = 1,
+        B = 2,
+        C = 3,
+        D = 4,
+        E = 5,
+        F = 6,
+        G = 7,
+        H = 8,
+        I = 9,
+    }
 }
