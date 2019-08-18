7bc9fcc7ac2706ac29cbbb864fd5d7ebe69d743a
LDZ <loic.dalzotto@hotmail.fr>
2019-08-18 08:46:49 +0200

    REFACTOR - Cleaner definition of frustum shape definition

--- Assets/~CoreGame/Math/ShapeDefinitions.cs
+++ Assets/~CoreGame/Math/ShapeDefinitions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace CoreGame
 {
@@ -16,29 +17,23 @@ namespace CoreGame
     {
         [SerializeField]
         public Vector3 Center;
-        [HideInInspector]
-        [SerializeField]
-        public Vector3 WorldPosition;
-        [HideInInspector]
-        [SerializeField]
-        public Quaternion WorldRotation;
         [SerializeField]
         public Quaternion DeltaRotation = Quaternion.identity;
-        [HideInInspector]
-        [SerializeField]
-        public Vector3 LossyScale;
         [SerializeField]
         public FrustumFaceV2 F1;
         [SerializeField]
         public FrustumFaceV2 F2;
         [SerializeField]
-        public Vector3 WorldStartAngleProjection;
-        [SerializeField]
         public float FaceDistance;
+        
+        
+        private Vector3 worldPosition;
+        private Quaternion worldRotation;
+        private Vector3 lossyScale;
+        private Vector3 worldStartAngleProjection;
+        private FrustumCalculationType frustumCalculationType;
 
-        [SerializeField]
-        public FrustumCalculationType FrustumCalculationType = FrustumCalculationType.PROJECTION;
-
+        public Quaternion WorldRotation { set => worldRotation = value; }
 
         public FrustumV2()
         {
@@ -48,29 +43,29 @@ namespace CoreGame
 
         public void SetCalculationDataForFaceBasedCalculation(Vector3 frustumWorldPosition, Quaternion frustumWorldRotation, Vector3 frustumLossyScale)
         {
-            this.WorldPosition = frustumWorldPosition;
-            this.WorldRotation = frustumWorldRotation;
-            this.LossyScale = frustumLossyScale;
-            this.FrustumCalculationType = FrustumCalculationType.FACE;
+            this.worldPosition = frustumWorldPosition;
+            this.worldRotation = frustumWorldRotation;
+            this.lossyScale = frustumLossyScale;
+            this.frustumCalculationType = FrustumCalculationType.FACE;
         }
 
         public void SetCalculationDataForPointProjection(Vector3 worldPositionPoint, Vector3 frustumWorldPosition, Quaternion frustumWorldRotation, Vector3 frustumLossyScale)
         {
-            this.WorldPosition = frustumWorldPosition;
-            this.WorldRotation = frustumWorldRotation;
-            this.LossyScale = frustumLossyScale;
-            this.WorldStartAngleProjection = worldPositionPoint;
-            this.FrustumCalculationType = FrustumCalculationType.PROJECTION;
+            this.worldPosition = frustumWorldPosition;
+            this.worldRotation = frustumWorldRotation;
+            this.lossyScale = frustumLossyScale;
+            this.worldStartAngleProjection = worldPositionPoint;
+            this.frustumCalculationType = FrustumCalculationType.PROJECTION;
         }
 
         public Quaternion GetRotation()
         {
-            return this.WorldRotation * this.DeltaRotation;
+            return this.worldRotation * this.DeltaRotation;
         }
 
         private Vector3 LocalToWorld(Vector3 localPoint)
         {
-            return (this.WorldPosition + this.WorldRotation * ((this.DeltaRotation * localPoint) + this.Center).Mul(this.LossyScale)).Round(3);
+            return (this.worldPosition + this.worldRotation * ((this.DeltaRotation * localPoint) + this.Center).Mul(this.lossyScale)).Round(3);
         }
 
         public void CalculateFrustumPoints(out Vector3 C1, out Vector3 C2, out Vector3 C3, out Vector3 C4, out Vector3 C5, out Vector3 C6, out Vector3 C7, out Vector3 C8)
@@ -81,7 +76,7 @@ namespace CoreGame
             C3 = this.LocalToWorld((new Vector3(this.F1.Width, -this.F1.Height, 0) + this.F1.FaceOffsetFromCenter).Mul(0.5f));
             C4 = this.LocalToWorld((new Vector3(-this.F1.Width, -this.F1.Height, 0) + this.F1.FaceOffsetFromCenter).Mul(0.5f));
 
-            if (this.FrustumCalculationType == FrustumCalculationType.FACE)
+            if (this.frustumCalculationType == FrustumCalculationType.FACE)
             {
                 C5 = this.LocalToWorld((new Vector3(-this.F2.Width, this.F2.Height, 0) + this.F2.FaceOffsetFromCenter).Mul(0.5f));
                 C6 = this.LocalToWorld((new Vector3(this.F2.Width, this.F2.Height, 0) + this.F2.FaceOffsetFromCenter).Mul(0.5f));
@@ -90,10 +85,10 @@ namespace CoreGame
             }
             else
             {
-                C5 = C1 + ((C1 - WorldStartAngleProjection) * this.FaceDistance);
-                C6 = C2 + ((C2 - WorldStartAngleProjection) * this.FaceDistance);
-                C7 = C3 + ((C3 - WorldStartAngleProjection) * this.FaceDistance);
-                C8 = C4 + ((C4 - WorldStartAngleProjection) * this.FaceDistance);
+                C5 = C1 + ((C1 - worldStartAngleProjection) * this.FaceDistance);
+                C6 = C2 + ((C2 - worldStartAngleProjection) * this.FaceDistance);
+                C7 = C3 + ((C3 - worldStartAngleProjection) * this.FaceDistance);
+                C8 = C4 + ((C4 - worldStartAngleProjection) * this.FaceDistance);
             }
         }
     }
